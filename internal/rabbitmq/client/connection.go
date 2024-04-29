aa400e0c14200e396e85e58abf693dbe5174969f
Reinier Bullain Escobar <rbullainescobar@corp.free.fr>
2024-04-29 11:40:48 +0200

    [AMQP] Adding IMessagingClient interface. Not convinced :(

--- internal/rabbitmq/client/connection.go
+++ internal/rabbitmq/client/connection.go
@@ -7,26 +7,37 @@ import (
 	"go-booking-system/internal/rabbitmq"
 )
 
+type IMessagingClient interface {
+	Connect(username, password, host, port, vhost string)
+	PublishOnQueue(payload rabbitmq.IPayload, queueName string) error
+	SubscribeToQueue(queueName, consumerName string, handler func(msg amqp.Delivery)) error
+	Close()
+}
+
 type RabbitMQConnection struct {
 	conn *amqp.Connection
 }
 
-func connect(username, password, host, port, vhost string) (*amqp.Connection, error) {
+var _ IMessagingClient = RabbitMQConnection{}
+
+func (connection RabbitMQConnection) Connect(username, password, host, port, vhost string) {
 	amqpUrl := fmt.Sprintf("amqp://%s:%s@%s:%s/%s", username, password, host, port, vhost)
 	conn, err := amqp.Dial(amqpUrl)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
-	return conn, nil
+	connection.conn = conn
 }
 
 func NewRabbitMQConnection(username, password, host, port, vhost string) RabbitMQConnection {
-	conn, err := connect(username, password, host, port, vhost)
-	if err != nil {
-		panic(err)
-	}
-	return RabbitMQConnection{
-		conn: conn,
+	conn := RabbitMQConnection{}
+	conn.Connect(username, password, host, port, vhost)
+	return conn
+}
+
+func (connection RabbitMQConnection) Close() {
+	if connection.conn != nil {
+		connection.conn.Close()
 	}
 }
 
@@ -64,5 +75,47 @@ func (connection RabbitMQConnection) PublishOnQueue(payload rabbitmq.IPayload, q
 		return err
 	}
 
+	return nil
+}
+
+func consume(msgs <-chan amqp.Delivery, handler func(msg amqp.Delivery)) {
+	for msg := range msgs {
+		handler(msg)
+	}
+}
+
+func (connection RabbitMQConnection) SubscribeToQueue(queueName, consumerName string, handler func(msg amqp.Delivery)) error {
+	ch, err := connection.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	queue, err := ch.QueueDeclare(
+		queueName,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	msgs, err := ch.Consume(
+		queue.Name,
+		consumerName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	go consume(msgs, handler)
 	return nil
 }
